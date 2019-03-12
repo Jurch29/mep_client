bee49a8e0b8b6ccb4c7b8a93ee1bcbaa3e426ac1
Jurch29 <julien.rch.29@gmail.com>
2019-03-12 11:29:02 +0100

    V1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import './App.css';
+import { Router, Route, Switch } from 'react-router-dom';
+import Navigbar from "./component/navbar/navbar";
+import history from "./component/history";
 
 class App extends Component {
 
     constructor(props){
         super(props)
 
+        /*
+        //exemple d'axios sur servlet :
         axios({
         method: 'post',
         url: 'http://localhost:8080/MEPDWP/ServletTest',
@@ -19,15 +24,19 @@ class App extends Component {
             mdp: 2
         }
         });
+        */
     }
 
     render() {
         return (
-            <div className="App">
-                
-                <p>LOLOLO</p>
-
-            </div>
+            <Router history={history}>
+            <div>
+                <Navigbar/>
+                <Switch>
+                    
+                </Switch>
+                </div>
+            </Router>
         );
     }
 }
